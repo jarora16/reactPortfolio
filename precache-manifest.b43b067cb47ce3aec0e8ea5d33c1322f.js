9e56066d22c0df9301db6c0d2a309f64acf40207
Jay Arora <arorajay640@gmail.com>
2020-12-03 19:54:00 -0500

    Updates

---
+++ precache-manifest.b43b067cb47ce3aec0e8ea5d33c1322f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0222ebc1d93caf59b6b2328c51bff7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4f0faa7c5055fbe445c",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "989ebaa290caff02536a",
+    "url": "/static/js/2.ca01c2d3.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.ca01c2d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f0faa7c5055fbe445c",
+    "url": "/static/js/main.581b2c27.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
